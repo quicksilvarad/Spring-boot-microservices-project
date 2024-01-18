3581f71389dcabc8b20155dd167a756b133bf51f
quicksilvarad <68800673+quicksilvarad@users.noreply.github.com>
2024-01-18 08:59:58 +0530

    Added API Client code implementation Added SpringCloudOpenFeign Dependency in POM

---
+++ employee-service/src/main/java/com/quicksilvarad/employeeservice/service/APIClient.java
@@ -0,0 +1,14 @@
+package com.quicksilvarad.employeeservice.service;
+
+import com.quicksilvarad.employeeservice.DTO.DepartmentDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(url="http://localhost:8080", value = "DEPARTMENT-SERVICE")
+public interface APIClient {
+    @GetMapping("api/departments/{code}")
+    DepartmentDTO getDepartment(@PathVariable("code") String departmentCode);
+}
