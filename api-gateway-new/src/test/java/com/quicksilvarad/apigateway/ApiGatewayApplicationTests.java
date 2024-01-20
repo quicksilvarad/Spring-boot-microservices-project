c23eaf7e7435f6d2ae7d5f9b461ca1a614c8e650
quicksilvarad <68800673+quicksilvarad@users.noreply.github.com>
2024-01-20 11:17:24 +0530

    Added API Gateway

---
+++ api-gateway-new/src/test/java/com/quicksilvarad/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quicksilvarad.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
