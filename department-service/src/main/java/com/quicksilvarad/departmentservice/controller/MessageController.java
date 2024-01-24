af629776ed95f2a45de514f963e827069403d4df
quicksilvarad <68800673+quicksilvarad@users.noreply.github.com>
2024-01-24 07:54:38 +0530

    implemented Spring cloud bus

---
+++ department-service/src/main/java/com/quicksilvarad/departmentservice/controller/MessageController.java
@@ -0,0 +1,20 @@
+package com.quicksilvarad.departmentservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class MessageController {
+
+    @Value("${spring.boot.message}")
+    String message;
+
+    @GetMapping("message")
+    public String message()
+    {
+        return message;
+    }
+}
